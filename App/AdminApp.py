2f62539017e1807b58db1b5eea9c62da45defca7
demxic <demxic@gmail.com>
2018-11-30 00:02:57 -0600

    refactor : Move code from the choose_trip_files to a new function

--- App/AdminApp.py
+++ App/AdminApp.py
@@ -1,16 +1,19 @@
-import sys, os
+import sys
 from pathlib import Path
+from FileParsers.filereaders import verify_files
 
+files = None
 
 class Menu:
     """Display a menu and respond to choices when run"""
 
     def __init__(self):
         self.choices = {
-            "1": self.read_trips_file,
-            "2": self.figure_out_unsaved_trips,
-            "3": self.search_for_trip,
-            "4": self.read_reserve_file,
+            "1": self.choose_trip_files,
+            "2": self.parse_trips_from_files,
+            "3": self.figure_out_unsaved_trips,
+            "4": self.search_for_trip,
+            "5": self.read_reserve_file,
             "10": self.quit}
 
     @staticmethod
@@ -36,15 +39,26 @@ class Menu:
             else:
                 print("{0} is not a valid choice".format(choice))
 
-    def read_trips_file(self):
+    def choose_trip_files(self):
+        """This option chooses the files that are to be read and turned into meaningful data
+
+        """
+        global files
         data_folder = Path("C:/Users/Xico/Google Drive/Sobrecargo/PBS/2018 PBS/201810 PBS")
-        file_to_open = data_folder / "201810 - PBS vuelos SOB A.txt"
-        import re
-        with open(file_to_open, 'r') as f:
-            content = f.read()
-            content_new = re.sub('=+\d+=+', '', content, flags=re.M)
-        with open(file_to_open, 'w') as f:
-            f.write(content_new)
+        file_names = ["201810 - PBS vuelos SOB A.txt", 
+                      "201810 - PBS vuelos SOB B.txt",
+                      "201810 - PBS vuelos EJE.txt"]
+        files = verify_files(data_folder, file_names)
+
+        # import re
+        # with open(file_to_open, 'r') as f:
+        #     content = f.read()
+        #     content_new = re.sub('=+\d+=+', '', content, flags=re.M)
+        # with open(file_to_open, 'w') as f:
+        #     f.write(content_new)
+        
+    def parse_trips_from_files(self):
+        print(files)
 
     def figure_out_unsaved_trips(self):
         pass
